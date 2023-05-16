b39b001e92a84117df51bd714d8fe07f1ad71be3
diegolavallade <66106864+diegolavallade@users.noreply.github.com>
2023-05-15 20:51:58 -0600

    Update Engine.cs
    - Now the colors are more exacts.
    - Added a Pause Function
    - If the image is larger than the window size, The image will be readjusted

--- Engine.cs
+++ Engine.cs
@@ -18,77 +18,155 @@ namespace YourNameSpace
             Console.SetBufferSize(width, height);
         }
 
-        public static string ObtenerColorMasCercano(Color color, Dictionary<Color, string> diccionario)
+        
+
+        private static string ObtenerColorMasCercano(Color color, Dictionary<Color, string> diccionario)
         {
             Dictionary<double, string> distancias = new Dictionary<double, string>();
             foreach (Color colorDicc in diccionario.Keys)
             {
-                int r1 = color.R;
-                int g1 = color.G;
-                int b1 = color.B;
-                int r2 = colorDicc.R;
-                int g2 = colorDicc.G;
-                int b2 = colorDicc.B;
-                double distancia = Math.Sqrt(Math.Pow((r1 - r2), 2) + Math.Pow((g1 - g2), 2) + Math.Pow((b1 - b2), 2));
+                double distancia = CalcularDistanciaLAB(color, colorDicc);
                 distancias[distancia] = diccionario[colorDicc];
             }
             return distancias[ObtenerMinimaDistancia(distancias.Keys)];
         }
 
-        public static double ObtenerMinimaDistancia(IEnumerable<double> distancias)
+        private static double CalcularDistanciaLAB(Color color1, Color color2)
         {
-            double minDistancia = double.MaxValue;
-            foreach (double distancia in distancias)
-            {
-                if (distancia < minDistancia)
-                {
-                    minDistancia = distancia;
-                }
-            }
-            return minDistancia;
+            double[] lab1 = RGBtoLAB(color1.R, color1.G, color1.B);
+            double[] lab2 = RGBtoLAB(color2.R, color2.G, color2.B);
+
+            double deltaL = lab1[0] - lab2[0];
+            double deltaA = lab1[1] - lab2[1];
+            double deltaB = lab1[2] - lab2[2];
+
+            return Math.Sqrt(deltaL * deltaL + deltaA * deltaA + deltaB * deltaB);
         }
 
-        public static List<string> ConvertirImagenATexto(string rutaImagen)
+        private static double[] RGBtoLAB(int r, int g, int b)
         {
-            Dictionary<Color, string> COLORS = new Dictionary<Color, string>()
-        {
-            {Color.FromArgb(0, 0, 0), "0"},                  // Negro
-            {Color.FromArgb(0, 55, 218), "1"},               // Azul
-            {Color.FromArgb(19, 161, 14), "2"},              // Verde
-            {Color.FromArgb(58, 150, 221), "3"},             // Aguamarina
-            {Color.FromArgb(197, 15, 31), "4"},              // Rojo
-            {Color.FromArgb(136, 23, 152), "5"},             // Púrpura
-            {Color.FromArgb(193, 156, 0), "6"},              // Amarillo
-            {Color.FromArgb(204, 204, 204), "7"},            // Blanco chafa
-            {Color.FromArgb(118, 118, 118), "8"},            // Gris
-            {Color.FromArgb(59, 120, 255), "9"},             // Azul claro
-            {Color.FromArgb(22, 198, 12), "A"},              // Verde claro
-            {Color.FromArgb(97, 214, 214), "B"},             // Aguamarina claro
-            {Color.FromArgb(231, 72, 86), "C"},              // Rojo claro
-            {Color.FromArgb(180, 0, 158), "D"},              // Púrpura claro
-            {Color.FromArgb(249, 241, 165), "E"},            // Amarillo palido
-            {Color.FromArgb(242, 242, 242), "F"}             // Blanco Brillante
-        };
+            double[] lab = new double[3];
 
-            Bitmap imagen = new Bitmap(rutaImagen);
-            int ancho = imagen.Width;
-            int alto = imagen.Height;
+            double[] xyz = RGBtoXYZ(r, g, b);
+            lab[0] = 116 * FuncionF(xyz[1]) - 16; // L*
+            lab[1] = 500 * (FuncionF(xyz[0] / 0.9505) - FuncionF(xyz[1])); // a*
+            lab[2] = 200 * (FuncionF(xyz[1]) - FuncionF(xyz[2] / 1.0890)); // b*
 
-            List<string> resultado = new List<string>();
-            for (int y = 0; y < alto; y++)
-            {
-                string linea = "";
-                for (int x = 0; x < ancho; x++)
-                {
-                    Color color = imagen.GetPixel(x, y);
-                    string colorValor;
-                    colorValor = COLORS.TryGetValue(color, out colorValor) ? colorValor : ObtenerColorMasCercano(color, COLORS);
-                    linea += colorValor + colorValor;
-                }
-                resultado.Add(linea);
-            }
-            return resultado;
+            return lab;
         }
+
+        private static double[] RGBtoXYZ(int r, int g, int b)
+        {
+            double[] xyz = new double[3];
+
+            double rLinear = r / 255.0;
+            double gLinear = g / 255.0;
+            double bLinear = b / 255.0;
+
+            rLinear = (rLinear > 0.04045) ? Math.Pow((rLinear + 0.055) / 1.055, 2.4) : (rLinear / 12.92);
+            gLinear = (gLinear > 0.04045) ? Math.Pow((gLinear + 0.055) / 1.055, 2.4) : (gLinear / 12.92);
+            bLinear = (bLinear > 0.04045) ? Math.Pow((bLinear + 0.055) / 1.055, 2.4) : (bLinear / 12.92);
+            xyz[0] = (rLinear * 0.4124564) + (gLinear * 0.3575761) + (bLinear * 0.1804375);
+            xyz[1] = (rLinear * 0.2126729) + (gLinear * 0.7151522) + (bLinear * 0.0721750);
+            xyz[2] = (rLinear * 0.0193339) + (gLinear * 0.1191920) + (bLinear * 0.9503041);
+
+            return xyz;
+        }
+
+        private static double FuncionF(double t)
+        {
+            double epsilon = 0.008856;
+            double kappa = 903.3;
+
+            if (t > epsilon)
+                return Math.Pow(t, 1.0 / 3.0);
+            else
+                return (kappa * t + 16.0) / 116.0;
+        }
+
+        private static double ObtenerMinimaDistancia(IEnumerable<double> distancias)
+            {
+                double minDistancia = double.MaxValue;
+                foreach (double distancia in distancias)
+                {
+                    if (distancia < minDistancia)
+                    {
+                        minDistancia = distancia;
+                    }
+                }
+                return minDistancia;
+            }
+
+        private static List<string> ConvertirImagenATexto(string rutaImagen)
+                {
+                    Dictionary<Color, string> COLORS = new Dictionary<Color, string>()
+            {
+                {Color.FromArgb(0, 0, 0), "0"},                  // Negro
+                {Color.FromArgb(0, 55, 218), "1"},               // Azul
+                {Color.FromArgb(19, 161, 14), "2"},              // Verde
+                {Color.FromArgb(58, 150, 221), "3"},             // Aguamarina
+                {Color.FromArgb(197, 15, 31), "4"},              // Rojo
+                {Color.FromArgb(136, 23, 152), "5"},             // Púrpura
+                {Color.FromArgb(193, 156, 0), "6"},              // Amarillo
+                {Color.FromArgb(204, 204, 204), "7"},            // Blanco chafa
+                {Color.FromArgb(118, 118, 118), "8"},            // Gris
+                {Color.FromArgb(59, 120, 255), "9"},             // Azul claro
+                {Color.FromArgb(22, 198, 12), "A"},              // Verde claro
+                {Color.FromArgb(97, 214, 214), "B"},             // Aguamarina claro
+                {Color.FromArgb(231, 72, 86), "C"},              // Rojo claro
+                {Color.FromArgb(180, 0, 158), "D"},              // Púrpura claro
+                {Color.FromArgb(249, 241, 165), "E"},            // Amarillo palido
+                {Color.FromArgb(242, 242, 242), "F"}             // Blanco Brillante
+            };
+
+                    Bitmap imagen = new Bitmap(rutaImagen);
+                    int ancho = imagen.Width;
+                    int alto = imagen.Height;
+
+                // Ajustar la imagen si supera el tamaño máximo permitido
+                if (ancho > (Console.BufferWidth/2) || alto > Console.BufferHeight)
+                    {
+                        double relacionAspecto = (double)ancho / alto;
+
+                        if (relacionAspecto > 2) // La imagen es más ancha que alta
+                        {
+                            ancho = Console.BufferWidth/2;
+                            alto = (int)(ancho / relacionAspecto);
+                        }
+                        else if (relacionAspecto == 0) //La imagen es cuadrada
+                        {
+                            ancho = Console.BufferHeight;
+                            alto = ancho;
+                        }
+                        else// La imagen es más alta que ancha
+                        {
+                            alto = Console.BufferHeight;
+                            ancho = (int)(alto * relacionAspecto);
+                        }
+
+                        imagen = new Bitmap(imagen, new Size(ancho, alto));
+                    }
+
+                    List<string> resultado = new List<string>();
+                    for (int y = 0; y < alto; y++)
+                    {
+                        string linea = "";
+                        for (int x = 0; x < ancho; x++)
+                        {
+                            Color color = imagen.GetPixel(x, y);
+                            string colorValor;
+                            colorValor = COLORS.TryGetValue(color, out colorValor) ? colorValor : ObtenerColorMasCercano(color, COLORS);
+                            linea += colorValor + colorValor;
+                        }
+                        resultado.Add(linea);
+                    }
+                    return resultado;
+                }
+
+        public static void Pause(int miliseconds)
+                {
+                Thread.Sleep(miliseconds);
+                }
 
         public static void Put(string str, int x, int y, bool Delay)
         {
